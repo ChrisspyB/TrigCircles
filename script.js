d4a645d94c9c2c9e43fd159014e490c1514748d1
ChrisspyB <christopher.bradley@live.co.uk>
2016-07-26 21:05:45 +0100

    Implement singleTrig rework into main page

--- script.js
+++ script.js
@@ -2,201 +2,290 @@
 'use strict'
 var width = 840;
 
-var SingleTrig = function(div_id,w,h,amp,cycles,phase,frames,fixed){
-	if(!fixed){
-	    amp = parseFloat(d3.select(div_id+'_amp').property('value'));	
-	    cycles =  parseFloat(d3.select(div_id+'_cycles').property('value'));	
-	    phase =  parseFloat(d3.select(div_id+'_phase').property('value'))*Math.PI;
-	}
-	var x = w/5, y=h/2, r=amp;
-	var tick=0;
-	var frame_length=100/6; // 60fps
-	var x_set = [], y_set = [];
-	var animating = false;
+var SingleTrig = function(div,x,w,h,a,c,p,frames) {
+	/*
+	Diagram involving a single circle.
+	x: center of circle. w,h: svg dimensions.
+	a,c,p = amplitude, cycles and phase of circle.
+		(--> draw: a*sin(cx+p))
+	frames = number of frames per cycle.
+	*/
+	// this._div	= div;
+	this._x 		= x;
+	this._y 		= h/2;
+	this._w			= w;
+	this._h 		= h;
+	this._a			= a;
+	this._c			= c;
+	this._p			= p;
+	this._frames	= frames;
+	this._tickmax	= frames; // separate copy of frames to allow updating
 
+	this._xs; // set of x coords of tracer
+	this._ys; // set of y coords of tracer
 
-	/*Note this 0 AND 2pi*/
-	for (var i=0; i<=frames; i++){
-		x_set.push(x+amp*Math.cos(phase+2*Math.PI*cycles*i/frames));
-		y_set.push(y-amp*Math.sin(phase+2*Math.PI*cycles*i/frames));
-	}
+	this.animating	= false;
+	this.tick 		= 0;
+	this.framelength= 100/6; //60fps
 
-	var svg = d3.select(div_id).insert("svg", ":first-child")
-		.attr('width',w)
-		.attr('height',h);
+	this.plotoffset	= 200;  
+	this.plotlength	= w-x-30;
+	
+	this._calculate();
 
-	var circle_main = svg.append('circle')
-		.attr('cx',x)
-		.attr('cy',y)
-		.attr('r',r);
-
-	var tracer = svg.append('circle')
-		.attr('cx',x_set[tick])
-		.attr('cy',y_set[tick])
+	this.svg = d3.select(div).insert("svg",":first-child")
+		.attr("width",w)
+		.attr("height",h);
+	//the main circle
+	this._circle = this.svg.append("circle")
+		.attr("cx",this._x)
+		.attr("cy",this._y)
+		.attr("r",this._a);
+	//small circle indicating current position on circle
+	this._tracer = this.svg.append('circle')
+ 		.attr('cx',this._xs[this.tick])
+		.attr('cy',this._ys[this.tick])
 		.attr('r',4)
 		.style('fill','black');
-
-	var line_radial = svg.append('line')
-		.attr('x1',x).attr('y1',y)
-		.attr('x2',x_set[0])
-		.attr('y2',y_set[0]);
-	var line_zero = svg.append('line')
-		.attr('x1',x).attr('y1',y)
-		.attr('x2',x_set[0])
-		.attr('y2',y_set[0]);
-
-	var arc = d3.svg.arc()
+	//line connecting tracer to origin
+	this._line_r = this.svg.append('line')
+		.attr('x1',this._x)
+		.attr('y1',this._y)
+		.attr('x2',this._xs[this.tick])
+		.attr('y2',this._ys[this.tick]);
+	//line indicating the starting phase
+	this._line_zero = this.svg.append('line')
+		.attr('x1',this._x)
+		.attr('y1',this._y)
+		.attr('x2',this._xs[0])
+		.attr('y2',this._ys[0]);
+	//inner arc indicating angle elapsed
+	this._arcfunc = d3.svg.arc()
 	    .innerRadius(0)
-	    .outerRadius(r/10)
-	    .startAngle(Math.PI/2-phase)
-	    .endAngle(Math.PI/2-phase)
-
-    var arc_path = svg.append('path')
-	    .attr("d",arc)
-	    .attr("transform", "translate("+x+","+y+")")
+	    .outerRadius(this._a/10)
+	    .startAngle(Math.PI/2-this._p)
+	    .endAngle(Math.PI/2-this._p);
+    this._arcpath = this.svg.append('path')
+	    .attr("d",this._arcfunc)
+	    .attr("transform", "translate("+this._x+","+this._y+")")
 	    .style('fill','blue')
-	    .style('opacity','0.5')
-
-    var arc_text = svg.append('text')
-    	.attr('x',x+2)
-    	.attr('y',y-10)
+	    .style('opacity','0.5');
+    this._arctxt = this.svg.append('text')
+    	.attr('x',this._x+2)
+    	.attr('y',this._y-10)
     	.text('')
-    	.style('fill','steelblue')
+    	.style('fill','steelblue');
 
-    var plot_scale_x = d3.scale.linear()
-	    .domain([0,frames])
-		.range([x+150+40,w-20]);
+    this._xscale = d3.scale.linear()
+	    .domain([0,this._tickmax])
+		.range([this._x+this.plotoffset,this._x+this.plotlength]);
 
-    var plot = svg.append('g');
+    this._plot = this.svg.append('g');
 
-	var graphF = d3.svg.line()
-		.x(function(d,i){return plot_scale_x(i);})
+	this._graphfunc = d3.svg.line()
+		.x(function(d,i){return this._xscale(i);})
 		.y(function(d){return d;})
 		.interpolate('basis');
 
-	var graph = plot.append('path')
+	this._graph = this._plot.append('path')
 		.classed('graph',true);
 
-	var line_y = svg.append('line')
-		.attr('x1',x_set[0]).attr('y1',y_set[0])
-		.attr('x2',plot_scale_x(0))
-		.attr('y2',y_set[0])
+	//line following the current x value
+	this._line_y = this.svg.append('line')
+		.attr('x1',this._xs[0])
+		.attr('y1',this._ys[0])
+		.attr('x2',this._xscale(0))
+		.attr('y2',this._ys[0])
 		.style('opacity','0');
-
-	var line_x = svg.append('line')
-		.attr('x1',plot_scale_x(0))
-		.attr('y1',y)
-		.attr('x2',plot_scale_x(0))
-		.attr('y2',y_set[0])
+	//line following the current y value
+	this._line_x = this.svg.append('line')
+		.attr('x1',this._xscale(0))
+		.attr('y1',this._y)
+		.attr('x2',this._xscale(0))
+		.attr('y2',this._ys[0])
 		.style('stroke','steelblue')
 		.style('opacity','0');
+	//x-axis for plot
+	this._axis_x = this.svg.append('line')
+		.attr('x1',this._xscale(0))
+		.attr('y1',this._y)
+		.attr('x2',this._xscale(this._tickmax))
+		.attr('y2',this._y);
 
-	/*Not using axis object as this is easier to animate*/
-	var axis_t = svg.append('line')
-		.attr('x1',plot_scale_x(0))
-		.attr('y1',y)
-		.attr('x2',plot_scale_x(frames))
-		.attr('y2',y);
-
-	var axis_text = [];
+	this._axistxt = [];
 
 	for (var i=0.5; i<=2; i+=0.5){
-		axis_text.push(
-			svg.append('text')
-	    	.attr('x',plot_scale_x(Math.round(i*frames)/2))
-	    	.attr('y',y+20)
+		this._axistxt.push(
+			this.svg.append('text')
+	    	.attr('x',this._xscale(Math.round(i*this._tickmax)/2))
+	    	.attr('y',this._y+20)
 	    	.text(i+'\u03C0')
 	    	.style('fill','darkred')
 	    	.style('text-anchor','middle')
 	    	.style('opacity','0')
-			);
+		);
 	}
-	var Animate = function(){
-		setTimeout(function(){
-			if(!animating){return;}
-			if(tick>frames){
-				tick=0;
-				animating=false;
-				line_x.style('opacity',0)
-				line_y.style('opacity',0)
-				axis_text[3].style('opacity','1');
-				return;
-			}
-			else if(tick===0){
-				line_x.style('opacity',1)
-				line_y.style('opacity',1)
-				axis_text[0].style('opacity','0')
-				axis_text[1].style('opacity','0')
-				axis_text[2].style('opacity','0')
-				axis_text[3].style('opacity','0')
-			}
-			else if(tick>3*frames/4){axis_text[2].style('opacity','1');}
-			else if(tick>frames/2){axis_text[1].style('opacity','1');}
-			else if(tick>frames/4){axis_text[0].style('opacity','1');}
-			tracer
-				.attr('cx',x_set[tick])
-				.attr('cy',y_set[tick])
 
-			line_radial
-				.attr('x2',x_set[tick])
-				.attr('y2',y_set[tick])
+	//set up onclick events
+	var that = this;
+	this.svg.on("click",function(){
+		that.animating=!that.animating;
+		if(that.animating){that._animate();}
+	})
 
-			var ang = tick*2*cycles*Math.PI/frames
-			arc_text.text((ang/Math.PI).toFixed(1)+"\u03C0");
-			arc.endAngle(Math.PI/2-ang-phase)
-			arc_path.attr("d",arc);
-
-			line_y
-				.attr('x1',x_set[tick])
-				.attr('y1',y_set[tick])
-				.attr('x2',plot_scale_x(tick))
-				.attr('y2',y_set[tick]);
-
-			line_x
-				.attr('x1',plot_scale_x(tick))
-				.attr('x2',plot_scale_x(tick))
-				.attr('y2',y_set[tick]);
-
-			graph.attr('d',graphF(y_set.slice(0,tick+1)))
-			tick++;
-			Animate();		
-		},frame_length);
-	};
-	d3.select(div_id).on('change',function(){
-		//Seperate variables to prevent messing with in progress animations.
-		var amp = parseFloat(d3.select(div_id+'_amp').property('value'));	
-		var cycles =  parseFloat(d3.select(div_id+'_cycles').property('value'));
-		var phase =  parseFloat(d3.select(div_id+'_phase').property('value'))*Math.PI;
-		d3.select(div_id).selectAll('label')
-			.data([(amp/100).toFixed(1),cycles.toFixed(1),(phase/Math.PI).toFixed(1)+"\u03C0"])
-			.text(function(d){return d;});
-			});
-	svg.on("click",function(){
-		if (animating) {
-			animating=false;
-		}
-		else {
-			if(tick==0 && !fixed){
-				//Update with new parameters
-			    amp = parseFloat(d3.select(div_id+'_amp').property('value'));	
-			    cycles =  parseFloat(d3.select(div_id+'_cycles').property('value'));
-			    phase =  parseFloat(d3.select(div_id+'_phase').property('value'))*Math.PI;
-			    
-			    x_set=[]; y_set=[];
-			    for (var i=0; i<=frames; i++){
-					x_set.push(x+amp*Math.cos(phase+2*Math.PI*cycles*i/frames));
-					y_set.push(y-amp*Math.sin(phase+2*Math.PI*cycles*i/frames));
-				}
-				r=amp;
-			    circle_main.attr('r',r);
-			    arc.outerRadius(r/10).startAngle(Math.PI/2-phase);
-			    line_zero.attr('x2',x_set[0]).attr('y2',y_set[0]);
-			}
-			animating=true;
-			Animate();
-		}
-	});
+	if(this.animating){this._animate();}
 };
+SingleTrig.prototype.rebuild = function() {
+	//Rebuild the image using new a,c,p,f etc...
+	this.animating = false;
+	this.tick=0;
+	this._tickmax = this._frames;
+	this._calculate();
+
+	this._circle.attr('r',this._a);
+	this._line_zero
+		.attr('x2',this._xs[0])
+		.attr('y2',this._ys[0]);
+	this._line_r
+		.attr('x2',this._xs[0])
+		.attr('y2',this._ys[0]);
+	this._tracer
+		.attr('cx',this._xs[0])
+		.attr('cy',this._ys[0]);
+	this._arcfunc
+	    .startAngle(Math.PI/2-this._p)
+	    .endAngle(Math.PI/2-this._p);
+	this._xscale.domain([0,this._tickmax])
+		.range([this._x+this.plotoffset,this._x+this.plotlength]);
+};
+SingleTrig.prototype._animate = function() {
+	var that = this;
+	setTimeout(function(){
+		that.update();
+	},this.framelength);
+};
+SingleTrig.prototype.update = function() {
+	if(!this.animating){return;}
+
+	if(this.tick===0){//first frame
+		this._line_x.style('opacity',1);
+		this._line_y.style('opacity',1);
+		this._axistxt[0].style('opacity','0');
+		this._axistxt[1].style('opacity','0');
+		this._axistxt[2].style('opacity','0');
+		this._axistxt[3].style('opacity','0');
+	}
+	else if (this.tick===Math.floor(this._tickmax/4)){
+		this._axistxt[0].style('opacity','1');
+	}
+	else if (this.tick===Math.floor(this._tickmax/2)){
+		this._axistxt[1].style('opacity','1');
+	}
+	else if (this.tick===Math.floor(3*this._tickmax/4)){
+		this._axistxt[2].style('opacity','1');
+	}
+	else if(this.tick>=this._tickmax) {
+		this.tick = 0; 
+		this.animating = false;
+
+		this._axistxt[3].style('opacity','1');
+		this._line_x.style('opacity',0);
+		this._line_y.style('opacity',0);
+
+		return;
+	}
+	this.tick++;
+	this._tracer
+		.attr('cx',this._xs[this.tick])
+		.attr('cy',this._ys[this.tick]);
+
+	this._line_r
+		.attr('x2',this._xs[this.tick])
+		.attr('y2',this._ys[this.tick])
+
+	var ang = this.tick*2*this._c*Math.PI/this._tickmax;
+	this._arctxt.text((ang/Math.PI).toFixed(1)+"\u03C0");
+	this._arcfunc.endAngle(Math.PI/2-ang-this._p);
+	this._arcpath.attr("d",this._arcfunc);
+
+	this._line_y
+		.attr('x1',this._xs[this.tick])
+		.attr('y1',this._ys[this.tick])
+		.attr('x2',this._xscale(this.tick))
+		.attr('y2',this._ys[this.tick]);
+
+	this._line_x
+		.attr('x1',this._xscale(this.tick))
+		.attr('x2',this._xscale(this.tick))
+		.attr('y2',this._ys[this.tick]);
+
+	this._graph.attr('d',this._graphfunc(this._ys.slice(0,this.tick+1)));
+	
+	this._animate();
+};
+SingleTrig.prototype._calculate = function(){
+	this._xs = [];
+	this._ys = [];
+	// console.log('building...');
+
+	for (var i=0; i<=this._tickmax; i++){
+		this._xs.push(
+			this._x+this._a*Math.cos(this._p+2*Math.PI*this._c*i/this._tickmax));
+		this._ys.push(
+			this._y-this._a*Math.sin(this._p+2*Math.PI*this._c*i/this._tickmax));
+	}
+}
+SingleTrig.prototype.setAmp = function(a){
+	//not visible until rebuilt
+	this._a = a;
+}
+SingleTrig.prototype.setCycle = function(c){
+	//not visible until rebuilt
+	this._c = c;
+}
+SingleTrig.prototype.setPhase = function(p){
+	//not visible until rebuilt
+	this._p = p;
+}
+SingleTrig.prototype.setFrames = function(f) {
+	//not visible until rebuilt
+	this._frames = f;
+};
+
+//div,x,y,w,h,a,c,p,frames
+var sin_trace = new SingleTrig("#sin_trace",200,840,280,100,1,0,60);
+var cos_trace = new SingleTrig("#cos_trace",200,840,280,100,1,Math.PI/2,60);
+var sin_i = new SingleTrig("#sin_i",200,840,320,
+	d3.select("#sin_i_a").property('value'),
+	d3.select("#sin_i_c").property('value'),
+	d3.select("#sin_i_p").property('value')*Math.PI,
+	60);
+
+
+// on change listeners
+
+d3.select("#sin_i_a").on("change",function(){
+	var val = parseFloat(d3.select(this).property('value'));
+	d3.select("#sin_i_a_label").text(val);	
+	sin_i.setAmp(val);
+	sin_i.rebuild();
+});
+d3.select("#sin_i_c").on("change",function(){
+	var val = parseFloat(d3.select(this).property('value'));
+	d3.select("#sin_i_c_label").text(val);	
+	sin_i.setCycle(val);
+	sin_i.rebuild();
+});
+d3.select("#sin_i_p").on("change",function(){
+	var val = parseFloat(d3.select(this).property('value'));
+	d3.select("#sin_i_p_label").text(val+"\u03C0");	
+	sin_i.setPhase(val*Math.PI);
+	sin_i.rebuild();
+});
+
+
+//--to be replaced--
+
 var MultiTrig = function(div_id,w,h,xOff,xDrawOff,amp,cycles,phase,frames,scannable){
 	var scan=false;
 	var x = w/5, y=h/2;
@@ -338,10 +427,6 @@ var MultiTrig = function(div_id,w,h,xOff,xDrawOff,amp,cycles,phase,frames,scanna
 
 	return {svg:svg,ani:animating};
 };
-SingleTrig('#small_sin_trace',width,280,280/3,1,0,60,true);
-SingleTrig('#small_cos_trace',width,280,280/3,1,Math.PI/2,60,true);
-SingleTrig('#sin_interactive',width,280*1.5,280/3,1,0,60,false);
-
 var fourier={
 	amp:{
 		sawtooth:[],
